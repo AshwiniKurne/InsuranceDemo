648a87d4eff939c911fac99c04ed8ed1eef9ad67
SanchayG <78357258+SanchayG@users.noreply.github.com>
2021-02-23 16:30:37 +0530

    Latest Code

--- TradePortalApp/src/views/MakerChecker/FileUpload.js
+++ TradePortalApp/src/views/MakerChecker/FileUpload.js
@@ -5,7 +5,10 @@ import axios from 'axios';
 import { Link } from 'react-router-dom';
 
 
+
 const FileUpload = () => {
+
+  
   const [file, setFile] = useState('');
   const [filename, setFilename] = useState('Choose File');
   const [fileType, setFileType] = useState('');
@@ -16,12 +19,16 @@ const FileUpload = () => {
   const [data, setData] = useState({});
   var check ='false';
 
+
   const onChange = e => {
     setFile(e.target.files[0]);
     setFilename(e.target.files[0].name);
     setFileType(e.target.files[0].type);
     setFileSize(e.target.files[0].size);
   };
+
+
+  
 
   const onSubmit = async e => {
     e.preventDefault();
@@ -49,7 +56,7 @@ const FileUpload = () => {
 
       setUploadedFile({ fileName, filePath });
 
-      setMessage('File Uploaded');
+      // setMessage('File Uploaded');
       console.log("after upload");
 
       setData({ fileName, fileType, fileSize, filePath });
@@ -69,19 +76,23 @@ const FileUpload = () => {
       for (var i = 0; i < data2.length; i++) {
         console.log("ash3 ", i);
         var td = document.createElement('td'); // create a td node
-        //console.log("data "+data.length);
-        // fill the td now with a piece of the data array
-        // document.getElementById("table-row").appendChild(td); //
-        //td.innerHTML = 'abcd';
-
+        
         if (i == data2.length - 1) {
 
           var td = document.createElement('td');
           var path = { filePath }.filePath;
 
           console.log("file path ", path);
-          td.innerHTML = '<a href= "' + path + '" target="_blank">View Document</a>'
+          // td.innerHTML = '<a href= "' + path + '" >View Document</a>'
           //td.innerHTML =<Link to={`/public/`+path}>View Document</Link>
+          // td.innerHTML ='<iframe src="https://view.officeapps.live.com/op/embed.aspx?src=http%3A%2F%2Fieee802%2Eorg%3A80%2Fsecmail%2FdocIZSEwEqHFr%2Edoc" width="100%" height="100%" frameborder="0">This is an embedded <a target="_blank" href="http://office.com">Microsoft Office</a> document, powered by <a target="_blank" href="http://office.com/webapps">Office Online</a>.</iframe>'
+          // td.innerHTML = '<iframe src="'+path+'">'
+          var button = document.createElement("button");
+          button.innerHTML = "View Document";
+          button.onclick = function(){
+            document.getElementById('viewDoc').src = path;
+          };
+          td.appendChild(button); 
           row.appendChild(td);
 
         } else {
@@ -91,8 +102,16 @@ const FileUpload = () => {
 
       }
       var td = document.createElement('td');
-      td.innerHTML = '<button onclick='+deleteRow(this)+'>Delete</button>';
 
+      var button = document.createElement("button");
+      button.innerHTML = "Delete";
+      button.onclick = function(){
+        alert('Delete Called');
+        // var btn = document.getElementById
+        // var row = btn.parentNode.parentNode;
+        // row.parentNode.removeChild(row);
+      };
+      td.appendChild(button); 
       row.appendChild(td);
       console.log("ash4");
       tableD.appendChild(row);
@@ -108,68 +127,78 @@ const FileUpload = () => {
 
     }
   };
-  function deleteRow(btn) {
-    if(check =='false'){
-      alert('check');
-    }else{
-      alert('Delete Row Called!!!');
-    }
-    
-//     console.log('inside delete');
-//   var row = btn.parentNode.parentNode;
-//   row.parentNode.removeChild(row);
-  }
+  
+  
 
   return (
     <Fragment>
       {message ? <Message msg={message} /> : null}
-      <form onSubmit={onSubmit}>
-        <div className='custom-file mb-4'>
-          <input
-            type='file'
-            className='custom-file-input'
-            id='customFile'
-            onChange={onChange}
-          />
-          <label className='custom-file-label' htmlFor='customFile'>
-            {filename}
-          </label>
-        </div>
-
-        <Progress percentage={uploadPercentage} />
-
-        <input
-          type='submit'
-          value='Upload'
-          className='btn btn-primary btn-block mt-4'
-        />
-      </form>
+      
       <br />
       <br />
-      <table id="docTable" style={{ width: '80%', border: '1px solid black', padding: '15px' }}>
-        <thead>
-          <tr style={{ color: 'white', backgroundColor: 'black' }}>
-            <th className='table-header'>File Name</th>
-            <th className='table-header'>File Type</th>
-            <th className='table-header'>File Size</th>
-            <th className='table-header'>View Document</th>
-            <th className='table-header'>Delete</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr className='table-row' id='table-row'></tr>
-        </tbody>
-      </table>
+      <table style={{width:'100%'}}>
+          <tr>
+                <td style={{width:'50%',height:'500px', border: '1px solid black'}}>
+                  <iframe id="viewDoc" style={{width:'100%',height:'500px'}}></iframe>
+                </td>
+                <td style={{width:'50%', border: '1px solid black'}}>
+                  <form onSubmit={onSubmit}>
+                    <div className='custom-file mb-4'>
+                      <input
+                        type='file'
+                        className='custom-file-input'
+                        id='customFile'
+                        onChange={onChange}
+                      />
+                      <label className='custom-file-label' htmlFor='customFile'>
+                        {filename}
+                      </label>
+                    </div>
+
+                    <Progress percentage={uploadPercentage} />
+
+                    <input
+                      type='submit'
+                      value='Upload'
+                      className='btn btn-primary btn-block mt-4'
+                    />
+                </form>
+                <br/>
+                  <table id="docTable" style={{ width: '100%', border: '1px solid black', padding: '15px' }}>
+                    <thead>
+                      <tr style={{ color: 'white', backgroundColor: 'black' }}>
+                        <th className='table-header'>File Name</th>
+                        <th className='table-header'>File Type</th>
+                        <th className='table-header'>File Size</th>
+                        <th className='table-header'>View Document</th>
+                        <th className='table-header'>Delete</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr className='table-row' id='table-row'></tr>
+                    </tbody>
+                  </table>
+                </td>
+            </tr>
+        </table>
       
 
+      
     </Fragment>
+    
   );
-
+  
 };
-function deleteRow(btn) {
-  console.log('inside delete');
-var row = btn.parentNode.parentNode;
-row.parentNode.removeChild(row);
-}
+
+
+
+      
+
+
+// function deleteRow(btn) {
+//   console.log('inside delete');
+// var row = btn.parentNode.parentNode;
+// row.parentNode.removeChild(row);
+// }
 
 export default FileUpload;
--- TradePortalApp/src/views/MakerChecker/test2.js
+++ TradePortalApp/src/views/MakerChecker/test2.js
@@ -2,12 +2,9 @@ import React from 'react';
 import FileUpload from './FileUpload';
 
 
-const test = () => (
-  <div className='container mt-4'>
-    <h4 className='display-4 text-center mb-4'>
-      <i className='fab fa-react' /> React File Upload
-    </h4>
 
+const test = () => (
+  <div>
     <FileUpload />
   </div>
 );
