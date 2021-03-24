9da9b898656d683d02adbb8a1065aecf8344f42d
AshwiniKurne <61222743+AshwiniKurne@users.noreply.github.com>
2021-03-24 13:06:55 +0530

    commit_24032021

--- TradePortalApp/src/views/dashboard/launchCase.js
+++ TradePortalApp/src/views/dashboard/launchCase.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState,useEffect } from 'react';
 import {
     CCard,
     CCardBody,
@@ -8,18 +8,57 @@ import {
     CLabel,
     CSelect,
     CProgress,
-    CFormGroup,CButton,CModal,CModalBody,CModalFooter
+    CFormGroup,CButton,CModal,CModalBody,CModalFooter,CInput
 } from '@coreui/react';
 import FileUpload from '../MakerChecker/FileUpload';
 import { Link } from 'react-router-dom';
 import CIcon from '@coreui/icons-react'
 const LaunchCase = () => {
 	const [modal, setModal] = useState(false);
-
+	const[value2,setValue2] =useState();
+    useEffect(() => { 
+        document.getElementById("copyCreate").style.display="none";
+        document.getElementById("new").style.display="none";
+    });
 	const toggle = ()=>{
     setModal(!modal);
 	}
 	
+	const search=()=>{
+    console.log("inside search"+value2); 
+	if(value2 =='copyCreate'){
+		document.getElementById("copyCreate").style.display="";
+        document.getElementById("new").style.display="none";
+	}else if(value2 =='New'){
+		document.getElementById("new").style.display="";
+        document.getElementById("copyCreate").style.display="none";
+	}else{
+		document.getElementById("firstDiv").style.display="";
+        document.getElementById("new").style.display="none";
+        document.getElementById("copyCreate").style.display="none";
+	}
+    
+	} 
+	
+	const handleChange =(e) =>{
+		
+		console.log("on change value ",e.target.value);
+        var val =e.target.value;
+
+        if(val ==0){
+            console.log("inside if 0");
+            setValue2('');
+        }else if(val ==1){
+            console.log("inside if 1");
+            setValue2('copyCreate');
+
+        }else if(val ==2){
+            console.log("inside if 2");
+            setValue2('New');
+
+        }
+
+	}
 
     return (
 
@@ -33,13 +72,45 @@ const LaunchCase = () => {
                             Launch Case
             </CCardHeader>
                         <CCardBody>
-                            <CFormGroup row className="my-0">
-
-                                <div className='container mt-4'>
+                            <CFormGroup >
+								<div id ='firstDiv'>
+								<CFormGroup row className="my-0">
+								<CCol>
+                                        <CLabel htmlFor="CopyCreate/New">Copy Create/New</CLabel>
+                                        <CSelect id="select" onChange={handleChange} >
+                                            <option value="0" >Please select </option>
+                                            <option value="1">Copy Create</option>
+                                            <option value="2">New</option>
+                                            
+                                        </CSelect>
+                                    </CCol>
+                                    <CCol>
+                                      <CButton color="warning" className="px-4" onClick={search.bind(this)} >
+									  <CIcon name="cilSearch" />&nbsp;Copy Create/New</CButton>
+                                        
+                                    </CCol>
+								</CFormGroup>
+								
+								</div>
+                                <br/><br/>
+                                <div id='copyCreate'>
+                                <CFormGroup row className="my-0">
+                                <CCol>
+                                        <CLabel htmlFor="Reference">Reference No</CLabel>
+                                        <CInput id="Reference" placeholder="Enter Reference No" />
+                                    </CCol>
+                                    <CCol>
+                                    <CButton color="warning" className="px-4" onClick={search.bind(this)} >
+									  <CIcon name="cilSearch" />&nbsp;Search</CButton>
+                                        
+                                    </CCol>
+                                </CFormGroup> 
+                                </div>
+                                <div className='container mt-4' id ='new'>
                                 <CFormGroup row className="my-0">
                                     <CCol>
                                         <CLabel htmlFor="Product">Product</CLabel>
-                                        <CSelect id="select"  >
+                                        <CSelect id="select" >
                                             <option value="0" >Please select Product</option>
                                             <option value="1">LC</option>
                                             <option value="2">BG</option>
